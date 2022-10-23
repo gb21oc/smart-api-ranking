927079c4fd65fec72bda24befdb1022218a430f1
Gabriel Jose <luckygb21@gmail.com>
2022-10-22 22:45:05 -0300

    (feat) Configurando o swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix("api")
+  // Document Swagger
+  const config = new DocumentBuilder()
+    .setTitle('Api Ranking')
+    //.setDescription('The cats API description')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
