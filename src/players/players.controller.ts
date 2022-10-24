5297c02a45b1d170923a88ff9a70ab113c62f4d6
Gabriel Jose <luckygb21@gmail.com>
2022-10-23 21:19:05 -0300

    (feat) Controller de player criada

--- src/players/players.controller.ts
+++ src/players/players.controller.ts
@@ -1,4 +1,27 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Logger, Post, Res, UsePipes } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
+import { CreatePlayerDTO } from './DTO/createPlayer.dto';
+import { Response } from "express"
+import { ValidateCreatePlayerPipe } from './pipe/validate-create-player.pipe';
 
-@Controller('players')
-export class PlayersController {}
+@ApiTags("Player")
+@Controller('player')
+export class PlayersController {
+    /**
+     *
+     */
+    constructor() {}
+
+    private readonly logger = new Logger(PlayersController.name)
+
+    @UsePipes(new ValidateCreatePlayerPipe())
+    @Post()
+    async post(@Body() body: CreatePlayerDTO, @Res() res: Response){
+        try {
+            //console.log(body)
+            return res.status(201).send({message: "Okk"})
+        } catch (e) {
+            this.logger.error(`[-] post: ${e.message}`)
+        }
+    }
+}
